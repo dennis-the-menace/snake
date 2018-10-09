6b1ed25849218bc0e0b77be5f17f2b8b1e3dd169
DennisRittmeyer <dennis.r@tamburin.ch>
2018-10-09 16:21:58 +0200

    initial project vip

---
+++ app/src/main/java/ch/games/dennisrittmeyer/snake/GameView.java
@@ -0,0 +1,30 @@
+package ch.games.dennisrittmeyer.snake;
+
+import android.content.Context;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private MainThread thread;
+
+    public GameView(Context context) {
+        super(context);
+
+        getHolder().addCallback(this);
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
---
+++ app/src/main/java/ch/games/dennisrittmeyer/snake/MainThread.java
@@ -0,0 +1,17 @@
+package ch.games.dennisrittmeyer.snake;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public class MainThread extends Thread{
+    private SurfaceHolder surfaceHolder;
+    private GameView gameView;
+    private boolean running;
+    public static Canvas canvas;
+
+    public MainThread(SurfaceHolder surfaceHolder, GameView gameView) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        thi
+    }
+}
